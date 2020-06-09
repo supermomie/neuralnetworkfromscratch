c40c7f97c6a516fbdc1f280ff8e401d22c1a1e92
supermomie <fakhredinedev3.0@gmail.com>
2020-06-09 23:26:25 +0200

    convert to API

--- nn.py
+++ nn.py
@@ -1,13 +1,8 @@
 import numpy as np
+import tensorflow as tf
+
 
 np.random.seed(0)
-
-
-"""
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-"""
 
 def create_data(points, classes):
     X = np.zeros((points*classes, 2))
@@ -23,9 +18,6 @@ def create_data(points, classes):
 X, y = create_data(2, 3)
 
 
-
-
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -37,12 +29,6 @@ class Layer_Dense:
 class Activation_ReLU:
 
     def forward(self, inputs):
-        #self.output = []
-        #for i in inputs:
-        #    if i > 0:
-        #        self.output.append(i)
-        #    elif i < 0:
-        #        self.output.append(0)
         self.output = np.maximum(0, inputs)
 
 
@@ -60,55 +46,21 @@ class Activation_Softmax:
 
 
 
-activation_relu = Activation_ReLU()
-activation_softmax = Activation_Softmax()
-
-#layer-1  # 2x10 with ReLU activation
-layer1 = Layer_Dense(2, 10)
-layer1.forward(X)
-activation_relu.forward(layer1.output)
-
-#layer-2  # 10x10 with softmax activation
-layer2 = Layer_Dense(10, 10)
-layer2.forward(layer1.output)
-activation_softmax.forward(layer2.output)
-
-
-#layer-3  # 10x2 with ReLU activation
-layer3 = Layer_Dense(10, 2)
-layer3.forward(layer2.output)
-activation_relu.forward(layer3.output)
-
-
-print('inputs values:')
-print(layer3.output)
-
-#print('activation ReLU ouput:')
-#print(activation_relu.output[:5])
-
-
-#print('activation Softmax ouput:')
-#print(activation_softmax.output[:5])
-
-
-
-
 
 
 
 
 
 """
-exp_values = np.exp(layer1.output)
-print('exponentiated values:')
-print(exp_values[:5])
+import math
 
 
-norm_values = exp_values / np.sum(exp_values)
-print('normalized exponentiated values:')
-print(norm_values[:5])
-print('sum of normalized values:', np.sum(norm_values))
+softmax_output = [0.7, 0.1, 0.2]  # example output from the output layer of the neural network.
+target_output = [1, 0, 0]  # ground truth
+
+loss = -(math.log(softmax_output[0])*target_output[0] +
+         math.log(softmax_output[1])*target_output[1] +
+         math.log(softmax_output[2])*target_output[2])
+
+print(loss)
 """
-
-
-
